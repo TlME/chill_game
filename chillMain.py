03abfd3778231d5ead5c53f4d32979d0d72b5a85
Nick <nicholas.henegar@gmail.com>
2016-11-28 15:54:52 -0800

    The game frame is done, time to tidy a few things up before final.

--- chillMain.py
+++ chillMain.py
@@ -3,11 +3,113 @@
 # Proudly presented by Nick Henegar
 # Written for Python v 2.7.12
 
+import madLists, random
+
+
 def mainGame(player_name=''):
     print("Welcome to Chill or Unchill, a text based adventure game where you make the important decisions.")
     if player_name == '':
         player_name = raw_input("\nFirst, what is your name, brave traveler? ").title()
-        if player_name == 'Snoop Dogg':
-            win(player_name)
     print("\nOk then, {}. You set out on your first adventure starting now. Keep your chill on lock.".format(player_name))
-    
+    #Begin game:
+    chill = 0
+    unchill = 0
+    paranoia = 0
+    alive = True
+    #Loop for game
+    while (alive):
+        q = procSit()
+        chill, unchill, paranoia = answer(chill, unchill, paranoia, q)
+        if unchill >= 10:
+            alive = False
+            print("You've reached a critical level of unchill. Karma kills you")
+        if chill >= 420:
+            alive = False
+            print("Wow, you're quite a patient person, {}. You made it to the end of the game!".format(player_name))
+    print("Final score: " + str(chill))
+    playAgain(player_name)
+    #end game
+
+    # Procedural Situation Gen
+    # @params - requires madLists to be loaded
+    # @returns - q, the sum of weighted chillness values
+def procSit():
+    #to-do: create list for encounter intro
+    string = ''
+    q = 0
+    for i in range(0,5):
+        j = random.randint(0,19)
+        string += madLists.procList[i][j][0]
+        q += madLists.procList[i][j][1]
+    print "\n" + string
+    return q
+
+    # Answer request and scoring
+def answer(chill, unchill, paranoia, q):
+    response = raw_input("What do you do?").lower()
+    if response == 'check stats':
+        print("\nchill points: " + str(chill) + "\nunchill points: " + str(unchill) + "\nparanoia level: " + str(paranoia))
+        answer(chill, unchill, paranoia, q)
+    if (random.randint(0,paranoia) <= 5) :
+        return normal(response, chill, unchill, paranoia, q)
+    else:
+        return paranoid(response, chill, unchill, paranoia, q)
+
+
+def normal(response, chill, unchill, paranoia, q):
+    print ("\nYou " + response + '.')
+    if (response == 'chill'):
+        if (q < -3) :
+            chill -= q
+            unchill -= q
+            print("\nThe situtation, however, is decidedly unchill. You have a bad time.")
+        else :
+            chill += q
+            print("\nThings are relatively chill, you end up enjoying yourself.")
+    elif (response == 'run'):
+        if (q > 1) :
+            unchill += q
+            print("\nVery unchill, friend. That's how you hurt people's feelings.")
+        else:
+            chill += 2
+            print("\nGood plan, that was spooky.")
+    elif (response == 'smoke'):
+        if (q < 0) :
+            chill -= q
+            paranoia -= q/2
+            print("\nThe situation is unchill, but you get so high that you no longer care. Unfortunately, you're sketched out by the end of it.")
+        else :
+            chill += (q + 2)
+            paranoia += q % 3
+            print("\nYou end up having a great time and making a new friend,\n but your new friend keeps talking about " + madLists.para[random.randint(0,14)][0] +
+                   ",\n which later give you some existential dread.")
+    return chill, unchill, paranoia
+
+def paranoid(response, chill, unchill, paranoia, q):
+    print("\nYou try to " + response + ", but you're feeling too paranoid.")
+    print("\nYou accuse them of " + madLists.para[random.randint(0,14)][0])
+    if (q < -5):
+        unchill -= 5 * q
+        print("\nYou're probably right, but now isn't a good time to say things like that. They react poorly.")
+    elif (-5 < q < 0):
+        unchill -= q
+        print("\nThe truth hurts. Kind of mean, but you couldn't help yourself.")
+    elif (0 <= q < 5):
+        unchill += 2 * q
+        print("\nThis earns you a nonplussed look as they leave you to your ravings.")
+    else:
+        chill += 3
+        paranoia -= 5
+        print("\nBut they laugh and think you are joking. Whew, that was close! You stop feeling as paranoid.")
+          
+    return chill, unchill, paranoia
+
+def playAgain(player_name):
+    choice = raw_input("\nWould you like to play again? (y/n) : ").lower()
+    if choice == 'y':
+        mainGame(player_name)
+    else:
+        print("I hope you enjoyed it!")
+        print 'Goodbye!'
+if __name__ == "__main__":
+    mainGame()
--- madLists.py
+++ madLists.py
@@ -1,43 +1,48 @@
 ## Question objects for mad-libbing
 # Designed to give the chillout.py game some added functionality
 #20 adjectives
-adjectives = ["An irascible ", "A frenzied ", "A terrified ", "A flatulent ", "A defiantly grotesque ",
-              "A shaky ", "A forlorn ", "An enchanted ", "A red-eyed ", "A pristine ",
-              "An alarmingly large ", "A perplexingly small ", "A radical ", "A dangerously pink ", "A perverse ",
-              "A furry ", "A three-toothed ", "A rapidly ballooning ", "A completely normal ", "An extremely racist"]
+procList = [[("An irascible ", 1), ("A frenzied ", 1), ("A terrified ", -1), ("A flatulent ", 2), ("A defiantly grotesque ", -2),
+              ("A shaky ", 0), ("A forlorn ", -1), ("An enchanted ", 1), ("A red-eyed ", 2), ("A pristine ", 2),
+              ("An alarmingly large ", 0), ("A perplexingly small ", 1), ("A radical ", 0), ("A dangerously pink ", 1), ("A perverse ", -1),
+              ("A furry ", 1), ("A three-toothed ", 1), ("A rapidly ballooning ", -1), ("A completely normal ", 0), ("An extremely racist ", -1)],
 #20 entities
-entity = ["skateboarder ","parrot ","telepathic swarm of bees ","old miner ","nurse ",
-          "version of your mother ","time-traveler ","hobo ","talking car ","plumber ",
-          "Snoop Dogg ","clone of you ","magical book ","5th-grade chess club ","series of increasingly absurd questions ","wizard ",
-          "pancake with writing on it ","possessed children's toy ","instance of your spirit animal ","Scientologist ","mirror"]
+            [("skateboarder, ", 1),("parrot, ", 1),("telepathic swarm of bees, ", -1),("old miner, ", 0),("nurse, ", 0),
+          ("version of your mother, ", -2),("time-traveler, ", 2),("hobo, ", -1),("talking car, ", -1),("plumber, ", 0),
+          ("Snoop Dogg, ", 3),("clone of you, ", -1),("magical book, ", -1),("5th-grade chess club, ", 1),("series of increasingly absurd questions, ", -1),("wizard, ", 0),
+          ("pancake with writing on it, ", 1),("possessed children's toy, ", -3),("instance of your spirit animal, ", 1),("Scientologist, ", -1),("mirror, ", 0)],
 #16 compositions
-composition = ["wearing ", "made of ", "covered in ", "throwing handfuls of ", "with a sack full of ", "whispering to their ",
-               "chewing on ","delicately caressing some ","completely unrelated to ","shoveling a path through the ","festooned with ",
-               "strangely lacking ","composed entirely out of ","lightly buttering some ","indecently eyeballing ","fresh out of"]
+                [("wearing ", 0), ("made of ", -1), ("covered in ", -1), ("throwing handfuls of ", 0), ("with a sack full of ", 1), ("whispering to their ", -1),
+               ("chewing on ", 1),("delicately caressing some ", -2),("completely unrelated to ", 0),("shoveling a path through the ", 1),("festooned with ", 2),
+               ("strangely lacking ", 1),("composed entirely out of ", 0),("lightly buttering some ", 1),("indecently eyeballing ", -1),("fresh out of ", 0),
+                 ("with a phobia of ", -2), ("buried under a pile of ", -1),("in tights patterned with little ", 2), ("extruding ", -3)],
 # 20 objects
-objects = ["leather shoes ", "gold bricks ", "hamster wheels ", "tinfoil-hats ", "metallic glitter ",
-           "lewd skeleton parts ","old newspapers ","colorful beads ","'legalize it' apparel ","grape jelly ",
-           "plastic feathers ","your self-esteem ","golf pencils ","playing cards ","trumpets ",
-           "unlit candles ", "wooden hammers ", "cereal boxes", "used chewing gum ", "404 errors"]
+            [("leather shoes, ", 0), ("gold bricks, ", 2), ("hamster wheels, ", 1), ("tinfoil-hats, ", -1), ("metallic glitter, ", 1),
+           ("lewd skeleton parts, ", -2),("old newspapers, ", 0),("colorful beads, ", 0),("'legalize it' apparel, ", -1),("grape jelly, ", 2),
+           ("plastic feathers, ", 1),("your self-esteem, ", -2),("golf pencils, ", 0),("playing cards, ", 0),("trumpets, ", 1),
+           ("unlit candles, ", -1), ("wooden hammers, ", 0), ("cereal boxes, ", 0), ("used chewing gum, ", -1), ("404 errors, ", 0)],
 # 20 actions
-action = ["offers you a handshake.", "pleads with you for their life.", "asks if you have change for a dollar.", "demands an end to Christmas.",
-          "does a kickflip over your car.", "makes a mockery of everything you value","accuses you of collaborating with banking conglomerates.",
-          "offers you a hit.", "shows you a painting and asks your opinion.", "teaches you several new gestural vulgarities.",
-          "stands motionless, saying nothing.","shoots paintballs at your knees.", "poses a difficult philsophical quandary."
-          "meets your eyes and mutters dejectedly, 'They know...' before exploding in a shower of gory bits.", "invites you to a party later",
-          "hands you a coloring book.", "transforms into no less than 17 cats, which then scatter into the alleys.",
-          "points at you while emitting a high-pitched keening noise to alert the others.", "wraps you in a big, bear-hug.", "just laughs."]
+            [("offers you a handshake.", 5), ("pleads with you for their life.", 7), ("asks if you have change for a dollar.", 1), ("demands an end to Christmas.", -1),
+          ("does a kickflip over your car.", 9), ("makes a mockery of everything you value.", -4),("accuses you of collaborating with banking conglomerates.", -3),
+          ("offers you a hit.", 10), ("shows you a painting and asks your opinion.", 2), ("teaches you several new gestural vulgarities.", -1),
+          ("stands motionless, saying nothing.", -2),("shoots paintballs at your knees.", -5), ("poses a difficult philosophical quandary.", 1),
+          ("meets your eyes and mutters dejectedly, 'They know...' before exploding in a shower of gory bits.", -7), ("invites you to a party later", 6),
+          ("hands you a coloring book.", 2), ("transforms into no less than 17 cats, which then scatter into the alleys.", -5),
+          ("points at you while emitting a high-pitched keening noise to alert the others.", -9), ("wraps you in a big, bear-hug.", 4), ("just laughs.", 5)],
 # 20 interrupts
-interrupt = ["turns to you and ", "stops and ", "makes a silly face and ","undulates wildly and ","dusts their shoulders off and ",
-             "looks up and ", "pauses and ", "thoughtfully sits and ", "does a quick double take and ", "jukes from side to side and ",
-             "plays a jaunty tune on an imaginary flute and ", "slowly peels away its flesh and ", "emerges from a chrysalis and ",
-             "pours a shot, downs it, and then ", "screeches loudly and ", "deploys a filthy meme and ", "plants an impressive garden and ",
-             "levitates briefly and ", "mumbles in a dark-tongue and ", "removes their hat and "]
+            [("turns to you and ", 0), ("stops and ", 0), ("makes a silly face and ", 3),("undulates wildly and ", -1),("dusts their shoulders off and ", 0),
+             ("looks up and ", 0), ("pauses and ", 0), ("thoughtfully sits and ", 1), ("does a quick double take and ", 0), ("jukes from side to side and ", 0),
+             ("plays a jaunty tune on an imaginary flute and ", 4), ("slowly peels away its flesh and ", -4), ("emerges from a chrysalis and ", -1),
+             ("pours a shot, downs it, and then ", 1), ("screeches loudly and ", -2), ("deploys a filthy meme and ", 2), ("plants an impressive garden and ", 3),
+             ("levitates briefly and ", 1), ("mumbles in a dark-tongue and ", -1), ("removes their hat and ", 0)]
+            ]
 # 15 paranoid
-paranoid = ["siphoning your thoughts for the mind-queen to feast upon.", "being a Trump supporter", "being a paid shill for the industry.",
-            "only pretending to be human.", "planting surveillance devices in your room", "boggarting.", "secretly being a reptilian robot in disguise.",
-            "stealing your used tissues.", "being a toilet-witch, fresh from India.", "being a blackula.", "working directly for Soros himself",
-            "leaving you fatherless for all these years.", "plotting to blow up your favorite coffee shop in a misguided attempt to point out systemic inequality.",
-            "actually being a skeleton in disguise.", "making you forget what you were about to say."]
+para =  [("siphoning your thoughts for the mind-queen to feast upon", 0), ("being a Trump supporter", 0), ("being a paid shill for the industry", 0),
+            ("only pretending to be human", 0), ("planting surveillance devices in your room", 0), ("boggarting", 0), ("secretly being a reptilian robot in disguise", 0),
+            ("stealing your used tissues", 0), ("being a toilet-witch, fresh from India", 0), ("being a blackula", 0), ("working directly for Soros himself", 0),
+            ("leaving you fatherless for all these years", 0), ("plotting to blow up your favorite coffee shop in a misguided attempt to point out systemic inequality", 0),
+            ("actually being a skeleton in disguise", 0), ("making you forget what you were about to say", 0)]
+            
+# Command List
+commands = ["chill", "run", "smoke"]
 
 
